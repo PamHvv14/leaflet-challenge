f718fa169dc0729e2e2515f0d7078f9de249817b
Pamela Hernandez <pamelahernandez@Pamelas-MacBook-Pro.local>
2020-11-29 13:05:22 -0800

    fixed

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,88 +1,98 @@
-// Store URL
-var earthquakes =  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// Store our API endpoint inside queryUrl
+var earthquakes_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-d3.json(earthquakes, function(earthquakeData) {  
-// Initialize & Create Two Separate LayerGroups: earthquakes & tectonicPlates
-  var earthquake = new L.LayerGroup()
-// Creating map object
-  var myMap = L.map("map", {
-    center: [37.7749, -122.4194],
-    zoom: 5
-  });
-// Adding tile layer
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createMap function
+  createMap(data.features);
+});
+
+
+function createMap(earthquakeData) {
+
+    // Loop through locations and markers elements
+      EarthquakeMarkers = earthquakeData.map((feature) =>
+                    //Yes, the geojson 'FORMAT' stores it in reverse, for some reason. (L.geojson parses it as [lat,lng] for you)
+                     //lat                         //long  
+        L.circleMarker([feature.geometry.coordinates[1],feature.geometry.coordinates[0]],{
+            radius: magCheck(feature.properties.mag),
+            stroke: true,
+            color: 'black',
+            opacity: 1,
+            weight: 0.5,
+            fill: true,
+            fillColor: magColor(feature.properties.mag),
+            fillOpacity: 0.9   
+        })
+        .bindPopup("<h1> Magnitude : " + feature.properties.mag +
+        "</h1><hr><h3>" + feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>")
+      )
+
+      // Add the earthquakes layer to a marker cluster group.
+      var earthquakes=L.layerGroup(EarthquakeMarkers)
+    //    console.log(d3.extent(d3.values(earthquakeData,((d) => +d.properties.mag))));
+       var mags = earthquakeData.map((d) => magCheck(+d.properties.mag));
+       console.log(d3.extent(mags));
+       console.log(mags);
+    //    console.log(earthquakeData.properties.mag);
+
+
+  // Define streetmap and darkmap layers
+  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
-   maxZoom: 18,
-   zoomOffset: -1,
-   id: "mapbox/streets-v11",
-   accessToken: "pk.eyJ1IjoicGFtZWxhMTQiLCJhIjoiY2toZTNvNmE0MGw2bjJ0bzI2bDZueHVoNSJ9.gwlPwxzhBSxgCLelb0g_Lw",
-  }).addTo(myMap);
-// Function to Determine Size of Marker Based on the Magnitude of the Earthquake
-  function markerSize(magnitude) {
-    if (magnitude === 0) {
-      return 1;
-    }
-      return magnitude * 3;
-    }
-// Function to Determine Style of Marker Based on the Magnitude of the Earthquake
-  function styleInfo(feature) {
-    return {
-      opacity: 1,
-      fillOpacity: 1,
-      fillColor: chooseColor(feature.properties.mag),
-      color: "#000000",
-      radius: markerSize(feature.properties.mag),
-      stroke: true,
-      weight: 0.5
-      };
-     }
-// Function to Determine Color of Marker Based on the Magnitude of the Earthquake
-  function chooseColor(magnitude) {
-    switch (true) {
-      case magnitude > 5:
-          return "#581845";
-      case magnitude > 4:
-          return "#900C3F";
-      case magnitude > 3:
-          return "#C70039";
-      case magnitude > 2:
-          return "#FF5733";
-      case magnitude > 1:
-          return "#FFC300";
-      default:
-          return "#DAF7A6";
-        }
-      }
-// Create a GeoJSON Layer
-      L.geoJSON(earthquakeData, {
-          pointToLayer: function(latlng) {
-              return L.circleMarker(latlng);
-          },
-          style: styleInfo,
-
-          onEachFeature: function(feature, layer) {
-              layer.bindPopup("<h4>Location: " + feature.properties.place + 
-              "</h4><hr><p>Date & Time: " + new Date(feature.properties.time) + 
-              "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
-          }
-      // Add earthquakeData to earthquakes LayerGroups 
-      }).addTo(earthquake);
-      // Add earthquakes Layer to the Map
-      earthquake.addTo(myMap);
-      // Set Up Legend
-      var legend = L.control({ position: "bottomright" });
-      legend.onAdd = function() {
-          var div = L.DomUtil.create("div", "info legend"), 
-          magnitudeLevels = [0, 1, 2, 3, 4, 5];
-          div.innerHTML += "<h3>Magnitude</h3>"
-          for (var i = 0; i < magnitudeLevels.length; i++) {
-              div.innerHTML +=
-                  '<i style="background: ' + chooseColor(magnitudeLevels[i] + 1) + '"></i> ' +
-                  magnitudeLevels[i] + (magnitudeLevels[i + 1] ? '&ndash;' + magnitudeLevels[i + 1] + '<br>' : '+');
-          }
-          return div;
-      };
-      // Add Legend to the Map
-      legend.addTo(myMap);
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken:  "pk.eyJ1IjoicGFtZWxhMTQiLCJhIjoiY2toZTNvNmE0MGw2bjJ0bzI2bDZueHVoNSJ9.gwlPwxzhBSxgCLelb0g_Lw"
   });
+
+  var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [streetmap, earthquakes]
+  });
+
+// Add a legend to the map
+var legend = L.control({ position: "bottomright" });
+
+legend.onAdd = function(myMap){
+    var div = L.DomUtil.create("div","legend");
+    div.innerHTML = [
+        "<k class='maglt2'></k><span>0-2</span><br>",
+        "<k class='maglt3'></k><span>2-3</span><br>",
+        "<k class='maglt4'></k><span>3-4</span><br>",
+        "<k class='maglt5'></k><span>4-5</span><br>",
+        "<k class='maggt5'></k><span>5+</span><br>"
+      ].join("");
+    return div;
+}
+
+legend.addTo(myMap);
+
+
+     function magColor(mag) {
+      var color = "";
+      if (mag <= 2) { color = "#ffffb2"; }
+      else if (mag <= 3) {color = "#fecc5c"; }
+      else if (mag <= 4) { color = "#fd8d3c"; }
+      else if (mag <= 5) {color = "#f03b20"; }
+      else { color = "#bd0026"; }
+    
+    return color;
+    
+    };
+// Function to determine if the magnitude is zero or less (See above discussion as it is possible to have
+// negative magnitudes, which obviously can't be used for setting the circleMarker radius)
+function magCheck(mag){
+  if (mag <= 1){
+      return 8
+  }
+  return mag * 8;
+};
+}
