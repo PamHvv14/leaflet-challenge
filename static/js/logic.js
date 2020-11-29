22162ce399df203ebe170de2b1f5f9ac6959f91e
PamHvv14 <68447115+PamHvv14@users.noreply.github.com>
2020-11-29 15:05:02 -0800

    TUNING DETAILS

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,21 +1,16 @@
-// Store our API endpoint inside queryUrl
+// URL
 var earthquakes_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 
-
-// Perform a GET request to the query URL
 d3.json(earthquakes_url, function(data) {
-  // Once we get a response, send the data.features object to the createMap function
   createMap(data.features);
 });
 
 
 function createMap(earthquakeData) {
 
-    // Loop through locations and markers elements
+    // Loop through locations 
       EarthquakeMarkers = earthquakeData.map((feature) =>
-                    //Yes, the geojson 'FORMAT' stores it in reverse, for some reason. (L.geojson parses it as [lat,lng] for you)
-                     //lat                         //long  
         L.circleMarker([feature.geometry.coordinates[1],feature.geometry.coordinates[0]],{
             radius: magCheck(feature.properties.mag),
             stroke: true,
@@ -31,16 +26,13 @@ function createMap(earthquakeData) {
         "</h3><hr><p>" + new Date(feature.properties.time) + "</p>")
       )
 
-      // Add the earthquakes layer to a marker cluster group.
+      // Add the earthquakes layer to a marker 
       var earthquakes=L.layerGroup(EarthquakeMarkers)
-    //    console.log(d3.extent(d3.values(earthquakeData,((d) => +d.properties.mag))));
-       var mags = earthquakeData.map((d) => magCheck(+d.properties.mag));
-       console.log(d3.extent(mags));
-       console.log(mags);
-    //    console.log(earthquakeData.properties.mag);
+      var mags = earthquakeData.map((d) => magCheck(+d.properties.mag));
+      console.log(d3.extent(mags));
+      console.log(mags);
 
-
-  // Define streetmap and darkmap layers
+  // Define streetmap
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -75,20 +67,17 @@ legend.onAdd = function(myMap){
 
 legend.addTo(myMap);
 
-
      function magColor(mag) {
       var color = "";
       if (mag <= 2) { color = "#FC4E2A"; }
       else if (mag <= 3) {color = "#CB2B3E"; }
       else if (mag <= 4) { color = "#2A81CB"; }
       else if (mag <= 5) {color = "#9C2BCB"; }
-      else { color = "#FFD326"; }
-    
+      else { color = "#FFD326"; }    
     return color;
-    
     };
-// Function to determine if the magnitude is zero or less (See above discussion as it is possible to have
-// negative magnitudes, which obviously can't be used for setting the circleMarker radius)
+  
+// Function to determine the magnitude
 function magCheck(mag){
   if (mag <= 1){
       return 4
